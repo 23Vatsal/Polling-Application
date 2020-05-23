32f0cdd8f6c0545cd9490f5d1bbe85d2513e6167
vatsal <vatsal@vatsal-VivoBook>
2020-05-23 13:49:03 +0530

    First Commit(TimeZone=UTC)

---
+++ src/test/java/com/pollingApp/pollingApp/PollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollingApp.pollingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
