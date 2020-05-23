434f569679e979660693f066c6f7a96057af75ce
vatsal <vatsal@vatsal-VivoBook>
2020-05-23 18:14:59 +0530

    Created DateAudit model

---
+++ src/main/java/com/pollingApp/pollingApp/config/AuditingConfig.java
@@ -0,0 +1,12 @@
+package com.pollingApp.pollingApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+	
+	//we will add more config later
+
+}
