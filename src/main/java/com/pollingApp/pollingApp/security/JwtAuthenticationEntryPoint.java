03937decee6787902236bf35fdb37f205a32966a
vatsal <vatsal@vatsal-VivoBook>
2020-05-24 20:31:42 +0530

    Adding classes for Spring Security

--- src/main/java/com/pollingApp/pollingApp/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/pollingApp/pollingApp/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,27 @@
 package com.pollingApp.pollingApp.security;
 
-public class JwtAuthenticationEntryPoint {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+	
+	private static final Logger logger= LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse httpServletResponse,
+			AuthenticationException e) throws IOException, ServletException {
+		
+		logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+		httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+		
+	}
 
 }
