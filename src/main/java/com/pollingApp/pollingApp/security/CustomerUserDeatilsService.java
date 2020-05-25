9ee3e3d65a49b9658e3725673be3489ce41fa0e8
vatsal <vatsal@vatsal-VivoBook>
2020-05-25 21:53:39 +0530

    Allows user to login using username or email

---
+++ src/main/java/com/pollingApp/pollingApp/security/CustomerUserDeatilsService.java
@@ -0,0 +1,40 @@
+package com.pollingApp.pollingApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pollingApp.pollingApp.models.User;
+import com.pollingApp.pollingApp.repositories.UserRepository;
+
+@Service
+public class CustomerUserDeatilsService implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		
+		//Let people login with username or email
+		User user=userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+						.orElseThrow(()-> new UsernameNotFoundException("User not found with username or email :"));
+		
+		return UserPrincipal.create(user);
+	}
+	
+	// This method is used by JWTAuthenticationFilter
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+}
