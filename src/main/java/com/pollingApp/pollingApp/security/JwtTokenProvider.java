a27cb8b38ac8f14cfd9bed13038e37fa9daf30ef
vatsal <vatsal@vatsal-VivoBook>
2020-05-25 22:11:40 +0530

    Utility class for generating and verifying JWT

---
+++ src/main/java/com/pollingApp/pollingApp/security/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+/*The following utility class will be used for generating a JWT after a user logs in successfully,
+and validating the JWT sent in the Authorization header of the requests
+*/
+package com.pollingApp.pollingApp.security;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenProvider {
+	
+	private static final Logger logger=LoggerFactory.getLogger(JwtTokenProvider.class);
+	
+	@Value("${app.jwtSecret}")
+	private String jwtSecret;
+	
+	@Value("${app.jwtExpirationInMs}")
+	private String jwtExpirationInMs;
+	
+	public String generateToken(Authentication authentication) {
+		
+		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+		
+		Date now = new Date();
+		String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+		DateFormat formatter = new SimpleDateFormat(DEFAULT_PATTERN);
+        Date expiryDate = null;
+		try {
+			expiryDate = formatter.parse(now.getTime() + jwtExpirationInMs);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Jwts.builder()
+                .setSubject(Long.toString(userPrincipal.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+	}
+	
+	
+	
+
+}
