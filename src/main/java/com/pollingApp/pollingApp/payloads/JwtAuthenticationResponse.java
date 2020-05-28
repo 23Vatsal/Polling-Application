31715078b4010bb37eda8af8bfb0c1f34282b040
vatsal <vatsal@vatsal-VivoBook>
2020-05-28 20:38:10 +0530

    Response Payloads

---
+++ src/main/java/com/pollingApp/pollingApp/payloads/JwtAuthenticationResponse.java
@@ -0,0 +1,31 @@
+package com.pollingApp.pollingApp.payloads;
+
+public class JwtAuthenticationResponse {
+	
+	private String accessToken;
+	
+	private String tokenType="bearer";
+
+	public JwtAuthenticationResponse(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+	
+	
+
+}
