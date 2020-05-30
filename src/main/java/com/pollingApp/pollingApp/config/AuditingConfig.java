2a527c02c2b0fe0e361b2ac9c6eb25818d534ff9
vatsal <vatsal@vatsal-VivoBook>
2020-05-30 22:12:01 +0530

    Auditing models and configuration

--- src/main/java/com/pollingApp/pollingApp/config/AuditingConfig.java
+++ src/main/java/com/pollingApp/pollingApp/config/AuditingConfig.java
@@ -1,12 +1,45 @@
 package com.pollingApp.pollingApp.config;
 
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.pollingApp.pollingApp.security.UserPrincipal;
 
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
 	
-	//we will add more config later
+	@Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new SpringSecurityAuditAwareImpl();
+    }
 
+}
+
+class SpringSecurityAuditAwareImpl implements AuditorAware<Long>{
+
+	@Override
+	public Optional<Long> getCurrentAuditor() {
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        
+        return Optional.ofNullable(userPrincipal.getId());
+		
+	}
+	
 }
