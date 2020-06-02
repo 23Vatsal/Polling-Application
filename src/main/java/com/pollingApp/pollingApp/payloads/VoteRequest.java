6b8e58676cf99ffadfa33c1fbcebc4a611ea759a
vatsal <vatsal@vatsal-VivoBook>
2020-06-02 21:58:03 +0530

    Request Payloads

---
+++ src/main/java/com/pollingApp/pollingApp/payloads/VoteRequest.java
@@ -0,0 +1,18 @@
+package com.pollingApp.pollingApp.payloads;
+
+import javax.validation.constraints.NotNull;
+
+public class VoteRequest {
+	
+	@NotNull
+    private Long choiceId;
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+
+}
